9180e4edd1e8f910b5cdbd9a469a3fed55ebc5f5
Maro <maronomoto@gmail.com>
2023-02-27 16:19:07 +0900

    api変更

--- script/main.js
+++ script/main.js
@@ -1,3 +1,3 @@
-  const url = 'https://api.open-meteo.com/v1/forecast?latitude=35.18&longitude=136.91&hourly=temperature_2m,precipitation,weathercode&timezone=Asia%2FTokyo';
+  const url = 'https://api.open-meteo.com/v1/jma?latitude=35.678&longitude=139.682&hourly=temperature_2m,weathercode&daily=weathercode,temperature_2m_max,temperature_2m_min&timezone=Asia%2FTokyo';
 
   fetch(url).then(data => data.json()).then(json => console.log(json));
